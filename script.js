9ca962a60fa9dad45441bfcd2ba74bf2498561df
zeuz <zeuz@zeuzs-iMac.local>
2023-04-13 16:35:04 -0400

    set up script file to update class name based on current hour, and save/get localstorage of the note saved for each hour

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 $(function () {
-  var current = moment().hours();
-  var time = $('#timeBlocks');
+  var current = dayjs().hour();
+  var time = $(".time-block");
+  var save = $(".saveBtn");
+  time.each(function () {
+    var hour = $(this).attr("id");
+    if (current < hour) {
+      $(this)
+        .children(".col-8")
+        .attr("class", "col-8 col-md-10 description future");
+    }
+    if (current > hour) {
+      $(this)
+        .children(".col-8")
+        .attr("class", "col-8 col-md-10 description past");
+    }
+    if (current == hour) {
+      $(this)
+        .children(".col-8")
+        .attr("class", "col-8 col-md-10 description present");
+    }
+  });
 
-  
+  save.on("click", function (event) {
+    event.preventDefault();
+    var key = $(this).parent().attr("id");
+    var value = $(this).siblings(".col-8").val().replace(key);
+
+    localStorage.setItem(key, JSON.stringify(value));
+  });
+
+  for (let i = 9; i <= 17; i++) {
+    $(`#${i} textarea`).val(JSON.parse(localStorage.getItem(`${i}`)));
+  }
 });
